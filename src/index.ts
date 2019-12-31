89ffe071df24bdfa0e77135d61045a5c7b1f99e1
Jack Fletcher <jdfletch97@gmail.com>
2019-12-30 20:15:27 -0600

    Add ribbonShots function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as bin} from "./bin";
 export {default as binShots} from "./binShots";
+export {default as ribbonShots} from "./ribbonShots";
 export {Point, Shot} from "./types";
---
+++ src/ribbonShots.ts
@@ -0,0 +1,27 @@
+import binByDistance from "./bin";
+import {Shot} from "./types";
+
+interface RibbonBin {
+  shootingPct: number;
+  width: number;
+}
+
+function ribbonShots(data: Shot[], maxDistance: number): RibbonBin[] {
+  const bins = binByDistance(data, maxDistance);
+  const totalShots = bins.reduce((a, b) => a + b.total, 0);
+
+  const ribbonBins = bins.map(bin => {
+    const shootingPct = bin.total > 0 ? bin.made / bin.total : 0;
+    const width = bin.total / totalShots;
+    return {
+      shootingPct,
+      width,
+    };
+  });
+
+  // TODO: implement smoothing function
+
+  return ribbonBins;
+}
+
+export default ribbonShots;
