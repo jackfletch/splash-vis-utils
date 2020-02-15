885a3bd7acf4fac24904cb4ae7e73388da1c248e
Jack Fletcher <jdfletch97@gmail.com>
2020-02-14 20:48:20 -0600

    Add left-right shot binning function

--- src/bin.ts
+++ src/bin.ts
@@ -20,4 +20,26 @@ function bin(shots: Shot[], maxDistance: number): Bin[] {
   return bins;
 }
 
+export function binLeftRight(shots: Shot[], maxDistance: number): Bin[][] {
+  const binsLeft: Bin[] = Array(maxDistance + 1)
+    .fill(undefined)
+    .map(() => ({made: 0, total: 0}));
+  const binsRight: Bin[] = Array(maxDistance + 1)
+    .fill(undefined)
+    .map(() => ({made: 0, total: 0}));
+
+  shots.filter(isLessThanDistance(maxDistance)).forEach(shot => {
+    const {distance, made, x} = shot;
+    if (x < 0) {
+      binsLeft[distance].total += 1;
+      binsLeft[distance].made += +made;
+    } else if (x > 0) {
+      binsRight[distance].total += 1;
+      binsRight[distance].made += +made;
+    }
+  });
+
+  return [binsLeft, binsRight];
+}
+
 export default bin;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {default as bin} from "./bin";
+export {default as bin, binLeftRight} from "./bin";
 export {default as binShots} from "./binShots";
 export {default as ribbonShots} from "./ribbonShots";
 export {Point, Shot} from "./types";
